1aa0666d5cabf44dbc6aa30c377e0a7dd255be3c
Nazrin Pashayeva <nazrin.pashayeva.std@bhos.edu.az>
2026-08-12 21:37:45 +0400

    Update get_entry_point.sh

--- re_fundamentals/get_entry_point.sh
+++ re_fundamentals/get_entry_point.sh
@@ -18,12 +18,26 @@ if ! readelf -h "$file_name" >/dev/null 2>&1; then
 fi
 
 magic_number=$(readelf -h "$file_name" | awk '/Magic:/ {
-    for (i = 2; i <= NF; i++)
+    for (i = 2; i <= NF; i++) {
         printf "%s%s", $i, (i == NF ? "\n" : " ")
+    }
 }')
-class=$(readelf -h "$file_name" | awk -F: '/Class:/ {gsub(/^[ \t]+/, "", $2); print $2}')
-byte_order=$(readelf -h "$file_name" | awk -F: '/Data:/ {gsub(/^[ \t]+/, "", $2); print $2}')
-entry_point_address=$(readelf -h "$file_name" | awk -F: '/Entry point address:/ {gsub(/^[ \t]+/, "", $2); print $2}')
+
+class=$(readelf -h "$file_name" | awk -F: '/Class:/ {
+    gsub(/^[ \t]+/, "", $2)
+    print $2
+}')
+
+byte_order=$(readelf -h "$file_name" | awk -F: '/Data:/ {
+    gsub(/^[ \t]+/, "", $2)
+    sub(/^2'\''s complement, /, "", $2)
+    print $2
+}')
+
+entry_point_address=$(readelf -h "$file_name" | awk -F: '/Entry point address:/ {
+    gsub(/^[ \t]+/, "", $2)
+    print $2
+}')
 
 source "$(dirname "$0")/messages.sh"
 display_elf_header_info
